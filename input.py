f4f4e0de3ea8726c30382bee20b521a4d91cc6c9
Sophia Lin <50805131+sophialin7@users.noreply.github.com>
2020-10-29 16:33:23 -0700

    Create input.py

---
+++ input.py
@@ -0,0 +1,5 @@
+# Welcome to the Puzzle Escape Room!
+# Solve these simple puzzles to complete the challenge!
+# First, we want to figure out why the program is not executing, and fix bugs in our code!
+
+print("hello")
